4848fa15171beda99c7c4ab225bbd027f0990077
emilbenev-hash <emil.b.enev@gmail.com>
2026-07-20 13:07:25 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    URL: 'https://xwnjuqqudkjkqdkrndhg.supabase.co',
+    KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3bmp1cXF1ZGtqa3Fka3JuZGhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5ODIzMTUsImV4cCI6MjA5ODU1ODMxNX0.CjTGOpfbETipi2HRegcGyvN9cGzSeQePFO0Sgc9Gt34'
+};
